f3300cf92f21810056aaac41bd59165527324366
Joe Wilson <joewilson502@gmail.com>
2021-03-12 19:38:56 +0000

    back button added

--- app/src/main/java/com/ecm2425/caweather/DailyActivity.java
+++ app/src/main/java/com/ecm2425/caweather/DailyActivity.java
@@ -5,11 +5,15 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class DailyActivity extends AppCompatActivity {
 
@@ -38,5 +42,26 @@ public class DailyActivity extends AppCompatActivity {
 
         dailyAdapter = new DailyAdapter(dailyWeatherResult, NUM_LIST_ITEMS);
         dailyWeatherList.setAdapter(dailyAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.back, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_back:
+                Context context = DailyActivity.this;
+                Class destinationActivity = MainActivity.class;
+                Intent intent = new Intent (context, destinationActivity);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
